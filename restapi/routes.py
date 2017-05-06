8358884b8c4979b7481b7c039eec803b5d163257
Thomas Wang <dev@paced.me>
2017-05-06 16:03:50 +1000

    Flask app is ready to go. Just need to test

--- restapi/routes.py
+++ restapi/routes.py
@@ -3,10 +3,33 @@
 from datetime import datetime
 
 from flask import Flask, jsonify, request, send_from_directory, url_for
-
-import secure
 from flask_sqlalchemy import SQLAlchemy
+
+import models
+import secure
 
 # Run the application.
 app = Flask(__name__)
 DEBUG = True
+
+# Directories for uploading media.
+app.config['PHOTOS_FOLDER'] = 'media/photos'
+app.config['VIDEOS_FOLDER'] = 'media/videos'
+app.config['MUSIC_FOLDER'] = 'media/music'
+
+# Database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = secure.DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app, use_native_unicode=True)
+
+# API Views, main route logic.
+
+
+@app.route("/api/test", methods=["GET, POST"])
+def testConnection():
+    """Display simple test route for the API."""
+    if request.method == "GET":
+        return "You sent a GET request and we received it. Congrats!"
+    else if request.method == "POST":
+        return "You sent a POST request and we received it. Congrats!"
